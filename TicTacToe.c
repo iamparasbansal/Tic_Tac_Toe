6a3f39681ff6905efe4ae53160a48b61c2789fd6
Paras Bansal <parasbansal685@gmail.com>
2021-01-22 07:38:01 +0530

    Menu Funtion added

--- TicTacToe.c
+++ TicTacToe.c
@@ -5,7 +5,7 @@
 
 int board[10] = {2,2,2,2,2,2,2,2,2,2};
 int turn = 1;
-int player;
+int player, comp;
 
 void menu();
 void go(int n);
@@ -24,8 +24,36 @@ void gotoxy(int x,int y)
 void main()
 {
     system("cls");
-
-
-
+    menu();
     getch();
+}
+
+void menu()
+{
+    int choice;
+    system("cls");
+    printf("\n--------MENU--------");
+    printf("\n1 : Play with X");
+    printf("\n2 : Play with O");
+    printf("\n3 : Exit");
+    printf("\nEnter your choice:>");
+    scanf("%d",&choice);
+    turn = 1;
+    switch (choice)
+    {
+    case 1:
+        player = 1;
+        comp = 0;
+        player_first();
+        break;
+    case 2:
+        player = 0;
+        comp = 1;
+        start_game();
+        break;
+    case 3:
+        exit(1);
+    default:
+        menu();
+    }
 }
