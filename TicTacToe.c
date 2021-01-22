b08f57ee336ca173ca9cf0ce7d7f1fad6192d636
Paras Bansal <parasbansal685@gmail.com>
2021-01-22 15:45:59 +0530

    Some Additions

--- TicTacToe.c
+++ TicTacToe.c
@@ -159,3 +159,32 @@ void go(int n)
         board[n] = 5;
     turn++;
 }
+
+void player_first()
+{
+    int pos;
+
+    check_draw();
+    draw_board();
+    gotoxy(30,18);
+    printf("Your Turn :> ");
+    scanf("%d",&pos);
+
+    if(board[pos] != 2)
+        player_first();
+
+    if(pos == posswin(player))
+    {
+        go(pos);
+        draw_board();
+        gotoxy(30,20);
+        //textcolor(128+RED);
+        printf("Player Wins");
+        getch();
+        exit(0);
+    }
+
+    go(pos);
+    draw_board();
+    start_game();
+}
